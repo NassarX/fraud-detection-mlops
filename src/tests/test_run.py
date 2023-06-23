0307e0e26f923b5807804b5b1679312164c02720
NassarX <mahmoud@nassarx.com>
2023-06-23 19:38:25 +0100

    Setup tests with samples to test data generations pipelines

--- src/tests/test_run.py
+++ src/tests/test_run.py
@@ -7,23 +7,97 @@ named ``test_*`` which test a unit of logic.
 
 To run the tests, run ``kedro test`` from the project root directory.
 """
-
-from pathlib import Path
-
+import numpy as np
 import pytest
+from kedro.config import ConfigLoader
+from kedro.framework.hooks import _create_hook_manager
+from pathlib import Path
 from kedro.framework.context import KedroContext
+
+from src.fraud_detection.pipelines.etl_app.nodes.data_generation import (
+    generate_customer_profiles_data,
+    generate_terminals_data
+)
 
 
 @pytest.fixture
-def project_context():
+def config_loader():
+    return ConfigLoader(conf_source=str(Path.cwd()))
+
+
+@pytest.fixture
+def project_context(config_loader):
     return KedroContext(
-        package_name="fraud_detection", project_path=Path.cwd()
+        package_name="fraud_detection",
+        project_path=Path.cwd(),
+        config_loader=config_loader,
+        hook_manager=_create_hook_manager(),
     )
 
 
-# The tests below are here for the demonstration purpose
-# and should be replaced with the ones testing the project
-# functionality
 class TestProjectContext:
-    def test_package_name(self, project_context):
-        assert project_context.package_name == "fraud_detection"
+    def test_project_path(self, project_context):
+        assert project_context.project_path == Path.cwd()
+        assert project_context._package_name == "fraud_detection"
+
+    def test_generate_customer_profiles_data(self, project_context):
+        # Define the expected column names in the generated dataframe
+        expected_columns = [
+            'CUSTOMER_ID',
+            'x_customer_id',
+            'y_customer_id',
+            'mean_amount',
+            'std_amount',
+            'mean_nb_tx_per_day'
+        ]
+
+        # Define the number of customers to generate
+        n_customers = 100
+
+        # Call the function to generate customer profiles data
+        customer_profiles_table = generate_customer_profiles_data(n_customers)
+
+        # Assert that the generated dataframe has the expected column names
+        assert list(customer_profiles_table.columns) == expected_columns
+
+        # Assert that the number of rows in the generated dataframe matches the number of customers
+        assert len(customer_profiles_table) == n_customers
+
+        # Assert that the generated dataframe does not contain any NaN values
+        assert not customer_profiles_table.isnull().values.any()
+
+        # Assert that the generated dataframe has the correct data types for each column
+        assert customer_profiles_table.dtypes['CUSTOMER_ID'] == np.int64
+        assert customer_profiles_table.dtypes['x_customer_id'] == np.float64
+        assert customer_profiles_table.dtypes['y_customer_id'] == np.float64
+        assert customer_profiles_table.dtypes['mean_amount'] == np.float64
+        assert customer_profiles_table.dtypes['std_amount'] == np.float64
+        assert customer_profiles_table.dtypes['mean_nb_tx_per_day'] == np.float64
+
+    def test_generate_terminals_data(self, project_context):
+        # Define the expected column names in the generated dataframe
+        expected_columns = [
+            'TERMINAL_ID',
+            'x_terminal_id',
+            'y_terminal_id'
+        ]
+
+        # Define the number of terminals to generate
+        n_terminals = 50
+
+        # Call the function to generate terminals data
+        terminal_data = generate_terminals_data(n_terminals)
+
+        # Assert that the generated dataframe has the expected column names
+        assert list(terminal_data.columns) == expected_columns
+
+        # Assert that the number of rows in the generated dataframe matches the number of terminals
+        assert len(terminal_data) == n_terminals
+
+        # Assert that the generated dataframe does not contain any NaN values
+        assert not terminal_data.isnull().values.any()
+
+        # Assert that the generated dataframe has the correct data types for each column
+        assert terminal_data.dtypes['TERMINAL_ID'] == np.int64
+        assert terminal_data.dtypes['x_terminal_id'] == np.float64
+        assert terminal_data.dtypes['y_terminal_id'] == np.float64
