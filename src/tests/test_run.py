8a6f746de7aa726bfa1a67d3af3350ae91eb70cf
corssinojaime <corssinojaime@gmail.com>
2023-06-25 14:23:57 +0100

    my tests

--- src/tests/test_run.py
+++ src/tests/test_run.py
@@ -8,6 +8,8 @@ named ``test_*`` which test a unit of logic.
 To run the tests, run ``kedro test`` from the project root directory.
 """
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 import pytest
 from kedro.config import ConfigLoader
 from kedro.framework.hooks import _create_hook_manager
@@ -16,7 +18,20 @@ from kedro.framework.context import KedroContext
 
 from src.fraud_detection.pipelines.etl_app.nodes.data_generation import (
     generate_customer_profiles_data,
-    generate_terminals_data
+    generate_terminals_data,
+    generate_dataset,
+    select_terminals_within_customer_radius
+    
+)
+
+from src.fraud_detection.pipelines.etl_app.nodes.data_exploration import (
+    plot_transactions_daily_stats,
+    plot_transactions_distribution
+)
+
+from src.fraud_detection.pipelines.etl_app.nodes.feature_transformation import (
+    get_customer_spending_behaviour_features
+    
 )
 
 
@@ -74,6 +89,8 @@ class TestProjectContext:
         assert customer_profiles_table.dtypes['std_amount'] == np.float64
         assert customer_profiles_table.dtypes['mean_nb_tx_per_day'] == np.float64
 
+    
+
     def test_generate_terminals_data(self, project_context):
         # Define the expected column names in the generated dataframe
         expected_columns = [
@@ -101,3 +118,143 @@ class TestProjectContext:
         assert terminal_data.dtypes['TERMINAL_ID'] == np.int64
         assert terminal_data.dtypes['x_terminal_id'] == np.float64
         assert terminal_data.dtypes['y_terminal_id'] == np.float64
+
+    
+    def test_generate_dataset_invalid_date(self, project_context):
+        n_customers = 100
+        n_terminals = 50
+        radius = 10.0
+        start_date = '2023/01/01'
+        nb_days = 7
+        
+        with pytest.raises(ValueError):
+            generate_dataset(n_customers, n_terminals, radius, start_date, nb_days)
+
+    
+
+
+def test_select_terminals_within_customer_radius(self, project_context):
+    # Create a customer profile
+    customer_profile = {
+        'x_customer_id': 10.0,
+        'y_customer_id': 10.0
+    }
+
+    # Create a DataFrame with terminal profiles
+    terminals_data = pd.DataFrame({
+        'x_terminal_id': [9.0, 10.0, 11.0, 12.0],
+        'y_terminal_id': [10.0, 11.0, 12.0, 13.0]
+    })
+
+    # Define the radius
+    radius = 5.0
+
+    # Expected result: Indices of terminals within the radius
+    expected_result = [0, 1]
+
+    # Call the function to select terminals within the customer's radius
+    result = select_terminals_within_customer_radius(customer_profile, terminals_data, radius)
+
+    # Compare the result with the expected result
+    assert result == expected_result
+
+def sample_transactions_data():
+    # Create a sample transactions dataframe
+    transactions_data = pd.DataFrame({
+        'CUSTOMER_ID': [1, 1, 1, 2, 2, 2, 3, 3, 3],
+        'TRANSACTION_ID': [101, 102, 103, 201, 202, 203, 301, 302, 303],
+        'TX_DATETIME': pd.to_datetime(['2023-06-01 09:00:00', '2023-06-05 14:00:00', '2023-06-10 10:00:00',
+                                       '2023-06-02 08:00:00', '2023-06-04 11:00:00', '2023-06-07 16:00:00',
+                                       '2023-06-03 13:00:00', '2023-06-06 15:00:00', '2023-06-10 18:00:00']),
+        'TX_AMOUNT': [50.0, 30.0, 20.0, 100.0, 150.0, 80.0, 70.0, 90.0, 120.0]
+    })
+    return transactions_data
+
+def test_get_customer_spending_behaviour_features(sample_transactions_data):
+    # Call the function under test
+    result = get_customer_spending_behaviour_features(sample_transactions_data, [1, 7, 30])
+
+    # Verify the output dataframe structure and values
+    assert result.columns.tolist() == ['CUSTOMER_ID', 'TRANSACTION_ID', 'TX_DATETIME', 'TX_AMOUNT',
+                                       'CUSTOMER_ID_NB_TX_1DAY_WINDOW', 'CUSTOMER_ID_AVG_AMOUNT_1DAY_WINDOW',
+                                       'CUSTOMER_ID_NB_TX_7DAY_WINDOW', 'CUSTOMER_ID_AVG_AMOUNT_7DAY_WINDOW',
+                                       'CUSTOMER_ID_NB_TX_30DAY_WINDOW', 'CUSTOMER_ID_AVG_AMOUNT_30DAY_WINDOW']
+    assert result['CUSTOMER_ID_NB_TX_1DAY_WINDOW'].tolist() == [1, 1, 1, 1, 2, 3, 1, 2, 3]
+    assert result['CUSTOMER_ID_AVG_AMOUNT_1DAY_WINDOW'].tolist() == [50.0, 30.0, 20.0, 100.0, 125.0, 83.33333333333333, 70.0, 60.0, 70.0]
+    assert result['CUSTOMER_ID_NB_TX_7DAY_WINDOW'].tolist() == [3, 3, 3, 3, 6, 6, 6, 6, 6]
+    assert result['CUSTOMER_ID_AVG_AMOUNT_7DAY_WINDOW'].tolist() == [33.333333333333336, 33.333333333333336, 33.333333333333336,
+                                                                     33.333333333333336, 95.0, 81.66666666666667,
+                                                                     80.0, 81.66666666666667, 93.33333333333333]
+    assert result['CUSTOMER_ID_NB_TX_30DAY_WINDOW'].tolist() == [3, 3, 3, 3, 6, 6, 6, 6, 6]
+    assert result['CUSTOMER_ID_AVG_AMOUNT_30DAY_WINDOW']
+
+
+def test_plot_transactions_distribution(sample_transactions_data):
+    # Call the function with example transactions
+    fig = plot_transactions_distribution(sample_transactions_data)
+
+    # Check if the function returns a matplotlib figure object
+    assert isinstance(fig, plt.Figure)
+
+    # Check if the figure has two subplots
+    assert len(fig.axes) == 2
+
+    # Check the properties of the first subplot
+    ax1 = fig.axes[0]
+    assert ax1.get_title() == 'Distribution of transaction amounts'
+    assert ax1.get_xlabel() == 'Amount'
+    assert ax1.get_ylabel() == 'Number of transactions'
+
+    # Check the properties of the second subplot
+    ax2 = fig.axes[1]
+    assert ax2.get_title() == 'Distribution of transaction times'
+    assert ax2.get_xlabel() == 'Time (days)'
+    assert ax2.get_ylabel() == 'Number of transactions'
+
+    # Add additional assertions if needed
+
+    # Clean up the plot after testing (optional)
+    plt.close(fig)
+
+
+
+def test_get_stats():
+    df = pd.read_csv("data/01_raw/04_transactions_data.csv")
+    assert df.dtypes['TRANSACTION_ID'] == np.int64
+    assert df.dtypes['TX_DATETIME'] == np.float64
+    assert df.dtypes['CUSTOMER_ID'] == np.float64
+    assert df.dtypes['TERMINAL_ID'] == np.float64
+    assert df.dtypes['TX_AMOUNT'] == np.float64
+    assert df.dtypes['TX_TIME_SECONDS'] == np.float64
+    assert df.dtypes['TX_TIME_DAYS'] == np.float64
+
+
+
+def test_plot_transactions_daily_stats():
+    # Create a sample transactions dataframe for testing
+    transactions = pd.DataFrame({
+        "TX_TIME_DAYS": [1, 2, 3, 4, 5],
+        "value": [10, 20, 15, 30, 25]
+    })
+
+    # Call the function to plot the daily statistics
+    fig = plot_transactions_daily_stats(transactions)
+
+    # Assertions to check the expected behavior of the function
+    assert isinstance(fig, plt.Figure)
+    assert fig.get_size_inches() == (20, 8)
+
+    # Check the plot elements
+    axes = fig.get_axes()
+    assert len(axes) == 1
+    ax = axes[0]
+    assert ax.get_title() == 'Total transactions, and number of fraudulent transactions \n and number of compromised cards per day'
+    assert ax.get_xlabel() == 'Number of days since beginning of data generation'
+    assert ax.get_ylabel() == 'Number'
+    assert ax.get_ylim() == (0, 300)
+
+    # Check the legend labels
+    legend = ax.legend_
+    assert legend.get_texts()[0].get_text() == '# transactions per day'
+    assert legend.get_texts()[1].get_text() == '# fraudulent txs per day'
+    assert legend.get_texts()[2].get_text() == '# fraudulent cards per day'
